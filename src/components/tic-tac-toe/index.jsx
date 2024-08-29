54841a5e6e725cc9224cbc33bb0dd3173daf0bfe
sadiq-1698 <mohammedsadiq1698@gmail.com>
2024-08-29 18:20:43 +0530

    update: display game win status

--- src/components/tic-tac-toe/index.jsx
+++ src/components/tic-tac-toe/index.jsx
@@ -14,6 +14,11 @@ const TicTacToe = () => {
 
   const togglePlayerTurn = () => setIsPlayerOne((prev) => !prev);
 
+  const restartGame = () => {
+    setGameWin(false);
+    setIsPlayerOne(true);
+  };
+
   const handleGridClick = (row, col) => {
     if (gameGrid[row][col] !== -1) return;
 
@@ -22,85 +27,66 @@ const TicTacToe = () => {
       copy[row][col] = isPlayerOne ? 1 : 0;
       return copy;
     });
+
     togglePlayerTurn();
   };
 
   useEffect(() => {
-    const isDiagonalCheck = (grid) => {
+    const checkWin = (grid) => {
+      const winCondition = (a, b, c) => a !== -1 && a === b && a === c;
+
+      for (let i = 0; i < 3; i++) {
+        if (
+          winCondition(grid[i][0], grid[i][1], grid[i][2]) ||
+          winCondition(grid[0][i], grid[1][i], grid[2][i])
+        ) {
+          return true;
+        }
+      }
+
       return (
-        (grid[0][0] === grid[1][1] &&
-          grid[0][0] === grid[2][2] &&
-          grid[0][0] !== -1) ||
-        (grid[0][2] === grid[1][1] &&
-          grid[0][2] === grid[2][0] &&
-          grid[0][2] !== -1)
+        winCondition(grid[0][0], grid[1][1], grid[2][2]) ||
+        winCondition(grid[0][2], grid[1][1], grid[2][0])
       );
     };
 
-    const isHorizontalCheck = (grid) => {
-      for (let i = 0; i < grid.length; i++) {
-        const valCheck = grid[i][0];
-        if (valCheck === -1) continue;
-        for (let j = 1; j < grid.length; j++) {
-          if (grid[i][j] !== valCheck) break;
-          if (j === grid.length - 1) {
-            console.log("came here");
-            return true;
-          }
-        }
-      }
-      return false;
-    };
-
-    const isVerticalCheck = (grid) => {
-      for (let i = 0; i < grid.length; i++) {
-        const valCheck = grid[0][i];
-        for (let j = 1; j < grid.length; j++) {
-          if (valCheck !== grid[i][j]) break;
-          if (i === grid.length - 1) return true;
-        }
-      }
-      return false;
-    };
-
-    if (
-      isDiagonalCheck(gameGrid) ||
-      isHorizontalCheck(gameGrid)
-      //  ||
-      //   isVerticalCheck(gameGrid)
-    ) {
+    if (checkWin(gameGrid)) {
       setGameWin(true);
     }
   }, [gameGrid]);
 
-  console.log(gameGrid);
-
   return (
     <div className="t3-grid-wrapper">
-      <div className="t3-grid">
-        {gameGrid.map((row, rowIdx) => {
-          return (
-            <div className="t3-grid-row">
-              {row.map((col, colIdx) => {
-                return (
-                  <div
-                    className="t3-grid-col"
-                    onClick={() => handleGridClick(rowIdx, colIdx)}
-                  >
-                    <div className="t3-grid-item">
-                      {col === 1 ? "✖️" : col === 0 ? "⚫" : ""}
-                      {/* {"[" + rowIdx + "]" + "[" + colIdx + "]"} */}
+      {gameWin ? (
+        <div className="game-over-wrapper">
+          <h1>{isPlayerOne ? "Player 2 Won" : "Player 1 Won"}</h1>
+          <br />
+          <button autoFocus={true} onClick={restartGame}>
+            Play again
+          </button>
+        </div>
+      ) : (
+        <div className="t3-grid">
+          {gameGrid.map((row, rowIdx) => {
+            return (
+              <div className="t3-grid-row">
+                {row.map((col, colIdx) => {
+                  return (
+                    <div
+                      className="t3-grid-col"
+                      onClick={() => handleGridClick(rowIdx, colIdx)}
+                    >
+                      <div className="t3-grid-item">
+                        {col === 1 ? "✖️" : col === 0 ? "⚫" : ""}
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        {gameWin ? (isPlayerOne ? "Player 2 Won" : "Player 1 Won") : ""}
-      </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
